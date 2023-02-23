e393d1e30dcac97f59e4debf2cd22131ff545943
pedromujica1 <107735941+pedromujica1@users.noreply.github.com>
2023-02-23 14:44:14 -0300

    Add files via upload

---
+++ PrimeiroProgramaJava.java
@@ -0,0 +1,15 @@
+//Classe impressão de texto: PrimeiroProgamaJava.java
+/* IFPR - Campus Cascavel
+Disciplina: Programação Orientada à Objetos
+Professor: Nelson Bellincanta */ 
+
+
+public class PrimeiroProgramaJava // declaracao da classe PrimeiroProgramaJava
+{
+    //declaração do método main, que dá início ao programa
+    public static void main (String[] args)
+    {
+        //impressão da mensagem
+        System.out.println("Primeiro Progama em Java!")
+    }//fim do método main
+}//fim da classe PrimeiroProgramaJava
