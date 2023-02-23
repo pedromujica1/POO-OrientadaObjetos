f5ecba51395cbf67aa04f9a3e126f683f72e2e1c
pedromujica1 <107735941+pedromujica1@users.noreply.github.com>
2023-02-23 14:45:24 -0300

    Update PrimeiroProgramaJava.java

--- PrimeiroProgramaJava.java
+++ PrimeiroProgramaJava.java
@@ -10,6 +10,6 @@ public class PrimeiroProgramaJava // declaracao da classe PrimeiroProgramaJava
     public static void main (String[] args)
     {
         //impressão da mensagem
-        System.out.println("Primeiro Progama em Java!")
+        System.out.println("Primeiro Progama em Java!");
     }//fim do método main
 }//fim da classe PrimeiroProgramaJava
