e04ca2542502f143a6d958ac96258d57ac40f509
pedromujica1 <miottopedro@gmail.com>
2023-05-11 19:28:22 -0300

    versão funcional

--- Java_Files/Poltronas.java
+++ Java_Files/Poltronas.java
@@ -12,9 +12,11 @@ import java.util.Scanner; //Importa a classe Scanner.
 public class Poltronas {//Início da classe Poltronas
     public static void main(String[]args){//Declaração e inicialização do metódo main
 
-        Boolean[] poltronas1 = {false,false,false,false,false};
+        boolean[] poltronas1 = {false,false,false,false,false};
 
-        Boolean[] poltronasEconomicas = {false,false,false,false,false};
+    
+
+        boolean[] poltronasEconomicas = {false,false,false,false,false};
 
         Scanner dados= new Scanner(System.in); //Inicia a classe Scanner para entrada de dados.
         
@@ -22,11 +24,13 @@ public class Poltronas {//Início da classe Poltronas
         int index2=0;
         boolean verificador = true;
 
+        
         while (verificador){
+            int poltransEco = 5;
 
-            System.out.println("Insira um número: "); //Imprime a mensagem de escolha.
-            System.out.println("Digite 1 para atribuir assentos na primeira classe ");
-            System.out.println("Digite 2 para atribuir assentos na classe econômica ");
+            System.out.println("Insira um número: "+ "\n"); //Imprime a mensagem de escolha.
+            System.out.println("Digite 1 para atribuir assentos na primeira classe "+ "\n");
+            System.out.println("Digite 2 para atribuir assentos na classe econômica "+ "\n");
             int input = dados.nextInt(); //Declara e inicia a variável input
             
 
@@ -39,9 +43,16 @@ public class Poltronas {//Início da classe Poltronas
                     System.out.println("A Primeira classe está cheia");
                     break;
                 }
-                System.out.println(Arrays.asList(poltronas1));
-                System.out.println("Poltronas: "+Arrays.asList(poltronas1));
-                System.out.println("Poltrona escolhida: "+poltronas1[index1]);
+
+                boolean[] result = new boolean[10];  //resultant array of size first array and second array  
+                System.arraycopy(poltronas1, 0, result, 0, poltronas1.length);  
+                System.arraycopy(poltronasEconomicas, 0, result, poltronas1.length,poltronasEconomicas.length);  
+                
+                System.out.println(Arrays.toString(result));    //prints the resultant array
+                
+                
+                
+                System.out.println("Poltrona escolhida: "+index1+"\n");
             }
 
             if (input ==2) {
@@ -50,14 +61,23 @@ public class Poltronas {//Início da classe Poltronas
                 poltronasEconomicas[index2]=true;
                 index2++;
 
+                
                 if (index2== 5){
                     System.out.println("A classe econômica está cheia");
                     System.out.println("Deseja comprar uma poltrara na classe 1?");
                     break;
                 }
-                System.out.print(Arrays.asList(poltronasEconomicas));
-                System.out.println("Poltronas: "+Arrays.asList(poltronasEconomicas));
-                System.out.println("Poltrona escolhida: "+poltronasEconomicas[index2]);
+
+                boolean[] result2 = new boolean[10];  //resultant array of size first array and second array  
+                System.arraycopy(poltronas1, 0, result2, 0, poltronas1.length);  
+                System.arraycopy(poltronasEconomicas, 0, result2, poltronas1.length,poltronasEconomicas.length);  
+                
+                System.out.println(Arrays.toString(result2));
+            
+                
+                System.out.println("Poltrona escolhida: "+ (index2+poltransEco)+"\n");
+
+                poltransEco--;
 
             }
            
