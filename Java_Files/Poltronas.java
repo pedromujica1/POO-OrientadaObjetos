870d54e2dcc107055a4717d63f22292801671d21
pedromujica1 <miottopedro@gmail.com>
2023-05-11 20:44:31 -0300

    so falta comentar e configurar assentos lotados

--- Java_Files/Poltronas.java
+++ Java_Files/Poltronas.java
@@ -14,19 +14,19 @@ public class Poltronas {//Início da classe Poltronas
 
         boolean[] poltronas1 = {false,false,false,false,false};
 
-    
-
         boolean[] poltronasEconomicas = {false,false,false,false,false};
 
         Scanner dados= new Scanner(System.in); //Inicia a classe Scanner para entrada de dados.
+        Scanner respostaLotado= new Scanner(System.in);
         
-        int index1=0;//
-        int index2=0;
         boolean verificador = true;
 
-        
+        int index1=0;
+        int index2=0;
+
         while (verificador){
             int poltransEco = 5;
+        
 
             System.out.println("Insira um número: "+ "\n"); //Imprime a mensagem de escolha.
             System.out.println("Digite 1 para atribuir assentos na primeira classe "+ "\n");
@@ -34,59 +34,90 @@ public class Poltronas {//Início da classe Poltronas
             int input = dados.nextInt(); //Declara e inicia a variável input
             
 
-            if (input == 1) {
+            if (input ==1 && index1<5) {
 
                 poltronas1[index1]=true;
                 index1++;
 
-                if (index1 == 5){
-                    System.out.println("A Primeira classe está cheia");
-                    break;
-                }
 
                 boolean[] result = new boolean[10];  //resultant array of size first array and second array  
                 System.arraycopy(poltronas1, 0, result, 0, poltronas1.length);  
                 System.arraycopy(poltronasEconomicas, 0, result, poltronas1.length,poltronasEconomicas.length);  
                 
-                System.out.println(Arrays.toString(result));    //prints the resultant array
+            
                 
                 
-                
+                System.out.println("Você escolheu a classe primária");
+
                 System.out.println("Poltrona escolhida: "+index1+"\n");
+
+                System.out.println("Gráfico de poltronas: "+Arrays.toString(result));
             }
 
-            if (input ==2) {
+
+            if (input == 1 && index1 >=5){
+                System.out.println("A Primeira classe está cheia");
+                System.out.println("Deseja comprar uma poltrara na classe 1? (s/n)");
+                String resposta1 = respostaLotado.nextLine();
+                System.out.println(resposta1);
+
+                
+                if (resposta1.equals("n")){
+                    System.out.println("O próximo voo parte em 3 horas.");
+                    break;
+                }
+            }
+
+            
+
+            if (input ==2 && index2<5) {
 
             
                 poltronasEconomicas[index2]=true;
                 index2++;
-
                 
-                if (index2== 5){
-                    System.out.println("A classe econômica está cheia");
-                    System.out.println("Deseja comprar uma poltrara na classe 1?");
-                    break;
-                }
+
+                System.out.println(index2);
+
 
                 boolean[] result2 = new boolean[10];  //resultant array of size first array and second array  
                 System.arraycopy(poltronas1, 0, result2, 0, poltronas1.length);  
                 System.arraycopy(poltronasEconomicas, 0, result2, poltronas1.length,poltronasEconomicas.length);  
                 
-                System.out.println(Arrays.toString(result2));
+                System.out.println("Você escolheu a classe econômica");
             
-                
                 System.out.println("Poltrona escolhida: "+ (index2+poltransEco)+"\n");
+
+                System.out.println("Gráfico de poltronas: "+Arrays.toString(result2)+"\n");
 
                 poltransEco--;
 
             }
+
+            if(input ==2 && index2 >=5){
+                System.out.println("A classe econômica está cheia");
+                System.out.println("Deseja comprar uma poltrara na classe 1? (s/n)");
+                String resposta = respostaLotado.nextLine();
+                
+
+                if (resposta.equals("s")){
+                    
+                    
+
+
+                }
+                if (resposta.equals("n")){
+                    System.out.println("O próximo voo parte em 3 horas".);
+                    break;
+                }
+                
+
+            }
+            
            
         }
         dados.close();
-        
-    
+        respostaLotado.close();
         
     }//Fim do metódo main
-
-}
-//Fim da classe Poltronas
+}//Fim da classe Poltronas
