918566751a14f903c2610c85b422e3f61bc691cd
Pedro Miotto Mujica <107735941+pedromujica1@users.noreply.github.com>
2023-10-19 11:53:16 -0300

    Create AppGUI01.java

---
+++ AppGUI01.java
@@ -0,0 +1,22 @@
+//Importar Java Swing
+package modeloJanela;
+
+import javax.swing.JFrame;
+
+public class AppGUI01 {
+
+	public static void main(String[] args) {
+		JFrame janela = new JFrame();
+		
+		janela.setSize(500,500);
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		janela.setVisible(true);
+		
+	
+		
+		
+		//
+
+	}
+
+}
